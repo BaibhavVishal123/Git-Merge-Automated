758758529d383e6ff62f39f57319389aae2bd668
Baibhav <baibhav@stratbeans.com>
2020-01-03 04:18:50 +0530

    add: ses fn

--- services/mail.js
+++ services/mail.js
@@ -1,12 +1,12 @@
 const nodemailer = require("nodemailer");
-const config = require("../config/config")
+const config = require("../config/config");
+var AWS = require('aws-sdk');
 
 function main(user, error) {
 
-    // async..await is not allowed in global scope, must use a wrapper
+    // TEST EMAIL with SES
     async function send(user, error) {
-        // Generate test SMTP service account from ethereal.email
-        // Only needed if you don't have a real mail account for testing
+        // Generate test SMTP service account from ethereal.email Only needed if you don't have a real mail account for testing
         let testAccount = await nodemailer.createTestAccount();
 
         // create reusable transporter object using the default SMTP transport
@@ -16,19 +16,11 @@ function main(user, error) {
             port: config.smtp.port,
             secure: config.smtp.secure, // true for 465, false for other ports
             auth: {
-                // user: testAccount.user, // generated ethereal user or config.smtp.user
-                // pass: testAccount.pass // generated ethereal password or config.smtp.password
-                user: config.smtp.auth.user,
-                password: config.smtp.auth.password
+                user: testAccount.user, // generated ethereal user or config.smtp.user
+                pass: testAccount.pass // generated ethereal password or config.smtp.password
             }
         });
 
-        var body = `<b>Please Merge latest commit manually to develop. <br><br><br>Error Stack Trace: </b><br><p>${error}</p>`;
-        if (typeof (err) === null) {
-            body = `<b>Please Merge latest commit manually to develop.`;
-        }
-
-        // send mail with defined transport object
         let info = await transporter.sendMail({
             from: '"Git Merge Bot 👻" <gitBot@stratbeans.com>', // sender address
             to: user.email, // list of receivers
@@ -42,14 +34,54 @@ function main(user, error) {
         });
 
         console.log("Message sent: %s", info.messageId);
-        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-        // Preview only available when sending through an Ethereal account
         console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
     }
 
-    send(user, error).catch(console.error);
+    async function sendSES(user, error) {
+        var ses = new AWS.SES({
+            "accessKeyId": config["smtp-ses"].auth.user,
+            "secretAccessKey": config["smtp-ses"].auth.password,
+            "region": config["smtp-ses"].region
+        });
+        var eparam = {
+            Destination: {
+                ToAddresses: [user.email]
+            },
+            Message: {
+                Body: {
+                    Html: {
+                        Data: `<b>Please Merge latest commit "${user.message},  ${user.commitId}"  
+                        manually to up(${config.git["repo-fake"].target})/down stream(${config.git["repo-fake"].source}).  
+                        <br><br><br><b>${error.message}</b><br><br>Error Stack Trace: <br><p><pre>${error.stack}</pre></p>`
+                    },
+                    Text: {
+                        Data: `<b>Please Merge latest commit "${user.message},  ${user.commitId}"  
+                        manually to up(${config.git["repo-fake"].target})/down stream(${config.git["repo-fake"].source}).  
+                        <br><br><br><b>${error.message}</b><br><br>Error Stack Trace: <br><p><pre>${error.stack}</pre></p>`
+                    }
+                },
+                Subject: {
+                    Data: "Automated GIT Merge Report ✔",
+                }
+            },
+            Source: '"Git Merge Bot 👻" <gitBot@stratbeans.com>',
+            ReplyToAddresses: ["baibhav@stratbeans.com"],
+            ReturnPath: "baibhav@stratbeans..com"
+        };
+
+        ses.sendEmail(eparam, function (err, data) {
+            if (err) console.log(err);
+            else console.log(data);
+        });
+    }
+
+    send(user, error).catch(function (err) {
+        console.error(err);
+    }, null);
+
+    sendSES(user, error).catch(function (err) {
+        console.error(err);
+    }, null);
 
 }
 
